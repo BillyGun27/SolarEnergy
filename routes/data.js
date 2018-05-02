d46ce497ec711b20abe90841e91ec8ea86b7fb30
Billy <billygun27@gmail.com>
2018-05-02 22:22:48 +0700

    array

--- routes/data.js
+++ routes/data.js
@@ -309,7 +309,7 @@ var pgsqlChain = function(box) {
 
 router.get('/saving/:id', function(request, response, next) {
   // callback//req.params
-      var query;
+      var query=[];
        query[0] = {
         text: "SELECT DISTINCT ON(tipe_energy) id,tipe_energy  , v::float*i::float AS watt, (SELECT (kapasitas_baterai::float *tegangan_baterai::float) FROM public.user_account WHERE id  = $1) AS battery,( v::float*i::float / (SELECT (kapasitas_baterai::float *tegangan_baterai::float) FROM public.user_account WHERE id  = 1) * 100 )AS batcap, to_char(receive_date, 'YY/MM/DD') AS receive_date,receive_time FROM energy WHERE tipe_energy = 'battery'  ORDER BY tipe_energy ,receive_date DESC,receive_time DESC ",
        values: [request.params.id]
