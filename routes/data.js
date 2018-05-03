d7e1d2233be5ff62507aac96c114404440e48f62
Billy <billygun27@gmail.com>
2018-05-04 01:30:06 +0700

    box batcap

--- routes/data.js
+++ routes/data.js
@@ -360,7 +360,7 @@ router.get('/rekomendasi/:id', function(request, response, next) {
 
        **/
       //console.log(successMessage);.batcap[0].batcap
-      response.send(box); 
+      response.send(box.batcap); 
     });
 
 });
